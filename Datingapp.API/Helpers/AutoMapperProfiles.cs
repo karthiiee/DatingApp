3fdd25eade44af3856abc2adf78fe85d6092c2bc
Karthikeyani Velavendhan <RBID009@rheinbrucke.loc>
2020-02-19 18:32:29 +0530

    End of Section8

---
+++ Datingapp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using AutoMapper;
+using Datingapp.API.Dtos;
+using Datingapp.API.Models;
+
+namespace Datingapp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>().
+              ForMember(dest => dest.PhotoUrl, opt => opt.
+                MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url)).
+                  ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<User, UserForDetailedDto>().
+              ForMember(dest => dest.PhotoUrl, opt => opt.
+                MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url)).
+                  ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotosForDetailedDto>();
+
+        }
+    }
+}
