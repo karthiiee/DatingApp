7b8d45c292a9a5616e55ba82d11b99eb20232957
Karthikeyani Velavendhan <RBID009@rheinbrucke.loc>
2020-03-02 12:34:14 +0530

    End of Section 13

--- Datingapp.API/Controllers/UsersController.cs
+++ Datingapp.API/Controllers/UsersController.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Datingapp.API.Data;
 using Datingapp.API.Dtos;
+using Datingapp.API.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Datingapp.API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
